d5ff9412b16c3dc6447f2879caa986a0c58c9dcc
UrangRobot <38397174+UrangRobot@users.noreply.github.com>
2019-01-14 16:33:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ setInterval(() => {
 
 music.on('message', async message => {
 
-    let prefix = 'mpi!';
+    let prefix = 'mpa!';
     let msg = message.content.toLowerCase();
     let args = message.content.slice(prefix.length).trim().split(" ");
     let cmd = args.shift().toLowerCase();
@@ -45,27 +45,22 @@ music.on('message', async message => {
         console.log(`${message.author.username} ran the command: ${cmd} on ${message.guild.name}`);
     }
 
-if (message.content === "mpi!join")
+if (message.content === "mpa!join")
   {
       message.member.voiceChannel.join()
                     .then(connection => {
     console.log("joined channel");
-      	const dispatcher = connection.playStream(ytdl(`https://www.youtube.com/watch?v=QfL-atdq5r4`))
+      	const dispatcher = connection.playStream(ytdl(`https://www.youtube.com/watch?v=hsr-Mu4Mdwk`))
         	connection.dispatcher.setVolumeLogarithmic(1);
     message.channel.send({embed: new Discord.RichEmbed()
                     .setAuthor(message.author.tag,message.author.avatarURL)
-                    .setDescription(`Music Player Indo, Joined Your Voice channel <a:a:532595746638397440>`)
+                    .setDescription(`Music Player Anime, Joined Your Voice channel <a:a:532595746638397440>`)
 .setColor("RANDOM")
                          })
 function tetew() {
     console.log("joined channel");
     const pokesamwan = [ //KATAKATAPOKESAMWAN
-        `https://www.youtube.com/watch?v=Pjxnery7i80`,
-        `https://www.youtube.com/watch?v=aV5ctic2m7c`,
-        `https://www.youtube.com/watch?v=ueFoN3CkeFs`,
-        `https://www.youtube.com/watch?v=CEykUQCktpQ`,
-        `https://www.youtube.com/watch?v=sbymJlnzOBU`,
-        `https://www.youtube.com/watch?v=BX7-IkC2HxY`
+        `https://www.youtube.com/watch?v=ndbtAZmIubY`
 
     ];
 const wordAnswer = pokesamwan[Math.floor(Math.random() * pokesamwan.length)]
@@ -77,7 +72,7 @@ const wordAnswer = pokesamwan[Math.floor(Math.random() * pokesamwan.length)]
                     .setDescription(`Radio Music Next <a:a:532595746638397440>`)
 .setColor("RANDOM")
                          })
-}; setInterval(tetew, 3900000)
+}; setInterval(tetew, 1800000)
                         
  })
 
@@ -89,15 +84,14 @@ music.login(process.env.TOKEN);
 music.on("ready", async () => {
 function botStatus() {  
 let status = [
-`mpi!join - mpi!leave`,
+`mpa!join - mpa!leave`,
 `in ${music.guilds.size} server.`,
 `24H active`,
 `https://discord.gg/3pe7c6h`
   ];
   let rstatus = Math.floor(Math.random() * status.length);
   music.user.setActivity(status[rstatus], {Type: 'STREAMING', url:'https://www.twitch.tv/verterid'});
-}; setInterval(botStatus, 10000) 
-   
+}; setInterval(botStatus, 10000)    
 console.log("ready");
     console.log(`Logged in as : ${music.user.tag}`);
     console.log(`${music.user.username} is ready!`)
